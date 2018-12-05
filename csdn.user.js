8f456236d6caf3975fa2ae550f9a4213af70eef1
zyr17 <jzjqz17@gmail.com>
2018-12-05 15:34:17 +0800

    remove adblock warning

--- csdn.user.js
+++ csdn.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CSDN Show Full Article
 // @namespace    none
-// @version      0.1.0
+// @version      0.1.1
 // @description  Automatically show full article in CSDN
 // @author       zyr17
 // @match        https://blog.csdn.net/*
@@ -11,4 +11,10 @@
     'use strict';
     $("div.article_content").removeAttr("style");
     $("#btn-readmore").parent().remove();
+    var checkid10000000 = setInterval(function (){
+        $('.check-adblock-bg').remove();
+        if ($('#check-adblock-time').size() == 0)
+            $('body').append($('<span id="check-adblock-time" style="opacity: 0">10000000</span>'));
+        clearInterval(checkid10000000);
+    }, 100);
 })();
